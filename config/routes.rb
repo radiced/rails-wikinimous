f51d9bac3ea538ff03d5d3b728856ac1f0bbd1e6
Cedric de Radzitzky <cderadzitzky@gmail.com>
2016-08-09 17:52:20 +0200

    rails wiki

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+
+  # get 'articles/show'
+
+  # get 'articles/new'
+
+  # get 'articles/edit'
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
